35c276e59c64dd945007fd5d7ee158832b29f4bf
Alexandr Rudkovskij <rudkovskiyalex@gmail.com>
2019-06-26 14:35:30 +0700

    fix

--- lfi-rfi/lfi-docker/app/controllers/user_controller.rb
+++
@@ -1,5 +0,0 @@
-class UserController < ApplicationController
-	def show
-		render params[:id]
-	end
-end
--- lfi-rfi/lfi-docker/config/routes.rb
+++
@@ -1,3 +0,0 @@
-RailsRce::Application.routes.draw do
-  get 'users/:id', to: 'user#show'
-end
